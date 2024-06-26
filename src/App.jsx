c72013df381543b5ff3b18a5e30e6ecba1b2c5b7
Hasan <158465711+Ahteshamull@users.noreply.github.com>
2024-06-26 14:12:07 +0600

    commit34

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
+import { MdDelete } from "react-icons/md";
+import { FaEdit } from "react-icons/fa";
+import { TiArrowBack } from "react-icons/ti";
+import { GrUpdate } from "react-icons/gr";
 import React, { useEffect, useState } from "react";
-import { getDatabase, ref, set, push, onValue } from "firebase/database";
+import {
+  getDatabase,
+  ref,
+  set,
+  push,
+  onValue,
+  remove,
+  update,
+} from "firebase/database";
 
 const App = () => {
+  let [id, setId] = useState("");
+  let [updatedTask, setUpdatedTask] = useState("");
+  let [edit, setEdit] = useState(false);
   let [task, setTask] = useState("");
   let [todos, setTodos] = useState([]);
   let handleTask = (e) => {
@@ -23,13 +38,32 @@ const App = () => {
     onValue(todosRef, (snapshot) => {
       let array = [];
       snapshot.forEach((item) => {
-        array.push(item.val());
+        array.push({ ...item.val(), key: item.key });
         setTodos(array);
       });
     });
     console.log(todos);
   }, []);
-
+  let handleDelete = (id) => {
+    const db = getDatabase();
+    remove(ref(db, "todos/" + id));
+  };
+  let handleEdit = (id) => {
+    setId(id);
+    setEdit(true);
+  };
+  let handleUpdate = (e) => {
+    setUpdatedTask(e.target.value);
+  };
+  let handleUpdateData = () => {
+    const db = getDatabase();
+    update(ref(db, "todos/" + id), {
+      name: updatedTask,
+    }).then(() => { 
+      setEdit(false);
+      alert("Okay");
+    });
+  };
   return (
     <div className="mx-auto w-[300px] text-center">
       <h1>Add Todo</h1>
@@ -47,11 +81,48 @@ const App = () => {
       >
         Submit
       </button>
-      <ul className="bg-gray-300 mt-5 border-[1px] border-black text-green-600 ">
+      <ul className="bg-gray-300 mt-5 border-[1px] border-black text-green-600 p-2">
         {todos.map((item) => {
-          return <li>{ item.name}</li>;
+          return (
+            <li className="mt-2">
+              {item.name}
+              <button
+                onClick={() => handleDelete(item.key)}
+                className="bg-red-500 text-white px-2  rounded-full ml-3"
+              >
+                <MdDelete />
+              </button>
+              <button
+                onClick={() => handleEdit(item.key)}
+                className="bg-green-500 text-white px-2  rounded-full ml-3"
+              >
+                <FaEdit />
+              </button>
+            </li>
+          );
         })}
       </ul>
+      {edit && (
+        <div className="w-[300px] py-10 bg-slate-600 rounded-md absolute top-0 left-2/4 translate-x-[-50%]">
+          <input
+            onChange={handleUpdate}
+            type="text"
+            placeholder="Update your data"
+          />
+          <button
+            onClick={handleUpdateData}
+            className="  bg-green-600 p-2 rounded-full ml-4"
+          >
+            <GrUpdate />
+          </button>
+          <button
+            onClick={() => setEdit(false)}
+            className=" absolute top-2 right-2 bg-red-600 p-2 rounded-full"
+          >
+            <TiArrowBack />
+          </button>
+        </div>
+      )}
     </div>
   );
 };
